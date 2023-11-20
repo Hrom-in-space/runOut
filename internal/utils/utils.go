9f117d615c5151a4377af0206c0da0c647148914
Artur Zenkov <hrom.not@gmail.com>
2023-11-20 04:10:00 +0400

    make async handler

---
+++ internal/utils/utils.go
@@ -0,0 +1,10 @@
+package utils
+
+func InSlice[T comparable](s []T, val T) bool {
+	for _, item := range s {
+		if item == val {
+			return true
+		}
+	}
+	return false
+}
--- main.go
+++ main.go
@@ -5,23 +5,33 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"log/slog"
 	"net"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
-	"github.com/sashabaranov/go-openai"
 
 	"runout/internal/db"
+	"runout/internal/utils"
 )
+
+// TODO: add Assistant worker
+// TODO: split to handlers/services/repositories
+// TODO: add tests
+// TODO: fix all slog messages
+// TODO: удалять треды через сутки в фоне
 
 func main() {
 	ctx := context.Background()
 	log.SetFlags(log.Ltime | log.Lshortfile)
 	// TODO: add config
+
+	audioChan := make(chan Audio, 1000)
 
 	// Database
 	psqlInfo := fmt.Sprintf(
@@ -43,12 +53,12 @@ func main() {
 	}
 
 	// OpenAI client
-	oaiClient := openai.NewClient(os.Getenv("OPENAI_API_KEY"))
+	// oaiClient := openai.NewClient(os.Getenv("OPENAI_API_KEY"))
 
 	// TODO: handler GET /needs
 	// TODO: handler POST /needs
 	router := mux.NewRouter()
-	router.Path("/needs").Methods(http.MethodPost).Handler(addNeed(dbPool, oaiClient))
+	router.Path("/needs").Methods(http.MethodPost).Handler(addNeed(audioChan))
 	router.Path("/needs").Methods(http.MethodGet).Handler(listNeeds(dbPool))
 
 	// TODO: graceful shutdown
@@ -67,42 +77,44 @@ func main() {
 	}
 }
 
-func addNeed(pool *sql.DB, oaiClient *openai.Client) http.HandlerFunc {
+func addNeed(ch chan<- Audio) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// TODO: add all openai supported formats
-		if r.Header.Get("Content-Type") != "audio/mp3" {
+		// TODO: security check real file type https://github.com/h2non/filetype
+		audioFormats := []string{
+			"flac",
+			"mp3",
+			"mp4",
+			"mpeg",
+			"mpga",
+			"m4a",
+			"ogg",
+			"wav",
+			"webm",
+		}
+
+		contentType := r.Header.Get("Content-Type")
+		format := strings.Replace(contentType, "audio/", "", 1)
+		if !utils.InSlice(audioFormats, format) {
+			slog.Error("wrong Content-Type", contentType)
 			w.WriteHeader(http.StatusBadRequest)
+			// TODO: return information about error
+			return
+		}
+
+		data, err := io.ReadAll(r.Body)
+		if err != nil {
+			slog.Error("ReadAll", err)
+			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 		defer r.Body.Close()
 
-		request := openai.AudioRequest{
-			Model:    "whisper-1",
-			FilePath: "need.mp3",
-			Reader:   r.Body,
-		}
-		resp, err := oaiClient.CreateTranscription(r.Context(), request)
-		if err != nil {
-			slog.Error("ListModels", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
+		ch <- Audio{
+			Data:   data,
+			Format: format,
 		}
 
-		// TODO: add request to Assistant
-
-		w.Header().Set("Content-Type", "application/json")
-		err = json.NewEncoder(w).Encode(resp.Text)
-		if err != nil {
-			slog.Error("Encode models", err)
-		}
-
-		// q := db.New(pool)
-		// err = q.CreateNeed(r.Context(), resp.Text)
-		// if err != nil {
-		// 	slog.Error("CreateNeed", err)
-		// 	w.WriteHeader(http.StatusInternalServerError)
-		// 	return
-		// }
+		w.WriteHeader(http.StatusOK)
 	}
 }
 
@@ -124,3 +136,38 @@ func listNeeds(pool *sql.DB) http.HandlerFunc {
 		}
 	}
 }
+
+type Audio struct {
+	Format string
+	Data   []byte
+}
+
+// func fn(oaiClient *openai.Client, pool *sql.DB) {
+// 	request := openai.AudioRequest{
+// 		Model:    "whisper-1",
+// 		FilePath: "need.mp3",
+// 		Reader:   r.Body,
+// 	}
+// 	resp, err := oaiClient.CreateTranscription(r.Context(), request)
+// 	if err != nil {
+// 		slog.Error("ListModels", err)
+// 		w.WriteHeader(http.StatusInternalServerError)
+// 		return
+// 	}
+//
+// 	// TODO: add request to Assistant
+//
+// 	w.Header().Set("Content-Type", "application/json")
+// 	err = json.NewEncoder(w).Encode(resp.Text)
+// 	if err != nil {
+// 		slog.Error("Encode models", err)
+// 	}
+//
+// 	// q := db.New(pool)
+// 	// err = q.CreateNeed(r.Context(), resp.Text)
+// 	// if err != nil {
+// 	// 	slog.Error("CreateNeed", err)
+// 	// 	w.WriteHeader(http.StatusInternalServerError)
+// 	// 	return
+// 	// }
+// }
